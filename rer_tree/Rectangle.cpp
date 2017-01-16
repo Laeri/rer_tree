c38bc91b07b5e8ed5ed915dbb38ba723370f7a75
laeri <lars.wuethrich@hotmail.com>
2017-01-16 20:23:24 +0100

    Intersect line segment with rectangle
    Check whether any side (segment) of the rectangle intersects with the given
    segment. Segments intersect if the two points of the other segment are on
    each side when looking from first point of rectangle segment to the other.
    This is the case when one is counterclockwise from rectangle segment and the
    other is clockwise. This can be determined by calculating the slope from one
    point to the next and from the next to the last. This segment intersection
    test is performed for all sides of the rectangle.

--- rer_tree/Rectangle.cpp
+++ rer_tree/Rectangle.cpp
@@ -9,3 +9,31 @@ Rectangle::Rectangle() {}
 Rectangle::Rectangle(float x_min, float x_max, float y_min, float y_max) : min(x_min, y_min), max(x_max, y_max) {}
 
 Rectangle::Rectangle(Point min, Point max) : min(min), max(max) {}
+
+bool ccw(Point a, Point b, Point c) {
+    float slope_comp = (b.y - a.y) * (c.x - b.x) - (c.y - b.y) * (b.x - a.x);
+    return slope_comp < 0;
+}
+
+bool segment_intersection(Point a, Point b, Point c, Point d) {
+    return (ccw(a, b, c) + ccw(a, b, d) == 1 && ccw(c, d, b) + ccw(c, d, a) == 1);
+}
+
+bool Rectangle::intersect(Point from, Point to) {
+    Point rect_from = Point(min.x, min.y);
+    Point rect_to = Point(max.x, min.y);
+    bool i_a = segment_intersection(rect_from, rect_to, from, to);
+    rect_from = rect_to;
+    rect_to.x = max.x;
+    rect_to.y = max.y;
+    bool i_b = segment_intersection(rect_from, rect_to, from, to);
+    rect_from = rect_to;
+    rect_to.x = min.x;
+    rect_to.y = max.y;
+    bool i_c = segment_intersection(rect_from, rect_to, from, to);
+    rect_from = rect_to;
+    rect_to.x = min.x;
+    rect_to.y = min.y;
+    bool i_d = segment_intersection(rect_from, rect_to, from, to);
+    return i_a || i_b || i_c || i_d;
+}
--- rer_tree/Rectangle.h
+++ rer_tree/Rectangle.h
@@ -14,7 +14,10 @@ public:
     Rectangle();
     Rectangle(float x_min, float x_max, float y_min, float y_max);
     Rectangle(Point min, Point max);
+    bool intersect(Point from, Point to);
 };
 
+bool ccw(Point a,Point b, Point c);
+bool segment_intersection(Point a, Point b, Point c, Point d);
 
 #endif //RER_TREE_RECTANGLE_H
