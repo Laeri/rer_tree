0f5a952a2838809172d441558bdb290b8f2f7eb2
laeri <lars.wuethrich@hotmail.com>
2017-01-16 18:48:42 +0100

    Draw selection rectangle when ctrl is down and mouse is moved
    This rectangle will be used to create obstacles for the rer_tree.

--- main.cpp
+++ main.cpp
@@ -23,21 +23,60 @@ int main() {
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
+    SDL_Rect tmp_rect;
+
     bool quit = false;
     SDL_Event e;
     Point current_mouse;
+    Point rect_begin;
+    bool ctrl_down = false;
+    bool draw_mouse_rect = false;
 
     while (!quit) {
         while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)) {
-                quit = true;
-            } else if (e.type == SDL_MOUSEMOTION) {
-                current_mouse.x = e.motion.x;
-                current_mouse.y = e.motion.y;
+            switch (e.type) {
+                case SDL_KEYDOWN:
+                    if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)) {
+                        quit = true;
+                    } else if (e.key.keysym.sym == SDLK_RCTRL || e.key.keysym.sym == SDLK_LCTRL) {
+                        ctrl_down = true;
+                    }
+                    break;
+                case SDL_KEYUP:
+                    if (e.key.keysym.sym == SDLK_RCTRL || e.key.keysym.sym == SDLK_LCTRL) {
+                        ctrl_down = false;
+                    }
+                    break;
+                case SDL_MOUSEMOTION:
+                    current_mouse.x = e.motion.x;
+                    current_mouse.y = e.motion.y;
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    if (ctrl_down) {
+                        draw_mouse_rect = true;
+                        int t_x, t_y;
+                        SDL_GetMouseState(&t_x, &t_y);
+                        rect_begin.x = t_x;
+                        rect_begin.y = t_y;
+                    }
+                    break;
+                case SDL_MOUSEBUTTONUP:
+                    draw_mouse_rect = false;
+                    break;
             }
         }
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
+
+        if (draw_mouse_rect) {
+            tmp_rect.w = current_mouse.x - rect_begin.x;
+            tmp_rect.h = current_mouse.y - rect_begin.y;
+            tmp_rect.x = rect_begin.x;
+            tmp_rect.y = rect_begin.y;
+            SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+            SDL_RenderDrawRect(renderer, &tmp_rect);
+        }
+
         SDL_RenderPresent(renderer);
     }
 
