4ce7afde126be5f59265a4c27e4acfe0952baf19
laeri <lars.wuethrich@hotmail.com>
2017-01-16 20:19:57 +0100

    Add rer_node for use in rer_tree
    Stores position and color which can be used to draw a visual representation
    to the screen. A node has a vector of children and also a pointer to its
    parent.

---
+++ rer_tree/rer_node.cpp
@@ -0,0 +1,34 @@
+//
+// Created by laeri on 1/16/17.
+//
+
+#include "rer_node.h"
+#include "math.h"
+
+rer_node::rer_node() {}
+
+rer_node::rer_node(float x, float y) : pos(x, y) {}
+
+rer_node::rer_node(Point &point) : pos(point.x, point.y) {}
+
+float rer_node::distance(const Point &other) {
+    float dx = other.x - pos.x;
+    float dy = other.y - pos.y;
+    return sqrt(dx * dx + dy * dy);
+}
+
+float rer_node::distance(const rer_node &node) {
+    float dx = node.pos.x - pos.x;
+    float dy = node.pos.y - pos.y;
+    return sqrt(dx * dx + dy * dy);
+}
+
+void rer_node::add_node(rer_node *node) {
+    children.push_back(node);
+}
+
+rer_node::~rer_node() {
+    for (rer_node *child: children) {
+        delete (child);
+    }
+}
---
+++ rer_tree/rer_node.h
@@ -0,0 +1,36 @@
+//
+// Created by laeri on 1/16/17.
+//
+
+#ifndef RER_TREE_NODE_H
+#define RER_TREE_NODE_H
+
+
+#include <vector>
+#include "Point.h"
+
+class rer_node {
+public:
+    Point pos;
+    int r, g, b, a;
+    bool colored = false;
+    std::vector<rer_node *> children;
+    rer_node *parent;
+
+    rer_node();
+
+    ~rer_node();
+
+    rer_node(float x, float y);
+
+    rer_node(Point &point);
+
+    float distance(const rer_node &node);
+
+    float distance(const Point &other);
+
+    void add_node(rer_node *node);
+};
+
+
+#endif //RER_TREE_NODE_H
